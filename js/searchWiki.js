ae8c3fdc28895cdb145bb34a1a07a9be6fa7ceeb
zelite <jcafernandes@gmail.com>
2016-08-13 13:51:18 +0200

    madeby and empty search results
    - add non breaking spaces in the "made by" span.
    - show message when the search terms have no results.
    -beautified js.

--- js/searchWiki.js
+++ js/searchWiki.js
@@ -1,52 +1,68 @@
-function searchWiki(){
-  var searchTerms = $("#search-bar").val();
-  if(searchTerms){
-    $.ajax({
-      url: "https://en.wikipedia.org/w/api.php?origin=*",
-      data: {
-        "action": "opensearch",
-        "search": searchTerms,
-        "format": "json"
-      },
-      type: "POST",
-      headers: {"Api-User-Agent": "WikipediaViewerZelite/1.0"},
-      success: populatePage,
-      error: function(data){
-        console.log(data);
-      }
-    });
-  }
+function searchWiki() {
+    var searchTerms = $("#search-bar").val();
+    if (searchTerms) {
+        $.ajax({
+            url: "https://en.wikipedia.org/w/api.php?origin=*",
+            data: {
+                "action": "opensearch",
+                "search": searchTerms,
+                "format": "json"
+            },
+            type: "POST",
+            headers: {
+                "Api-User-Agent": "WikipediaViewerZelite/1.0"
+            },
+            success: populatePage,
+            error: function(data) {
+                console.log(data);
+            }
+        });
+    }
 
 }
 
-function clickDivFollowLink(url){
-  return function(){
-    window.location.href = url;
-  };
+function clickDivFollowLink(url) {
+    return function() {
+        window.location.href = url;
+    };
 }
 
 
-function populatePage(data){
-  //first clear any previous results
+function populatePage(data) {
+    //first clear any previous results
     $("#search-results").empty();
-  for(var i = 0; i < data[1].length; i++){
-    //Build the components of each new div
-    var title = $("<h2/>", {text: data[1][i]});
-    var summary = $("<p/>", {text: data[2][i]});
-    var url = data[3][i];
+    if (data[1].length === 0) {
+        $("#search-results").append(
+            $("<div/>", {
+                class: "noresult"
+            })
+            .html("No results found for <strong>" + data[0] + "</strong>. Try searching for something else."));
+    } else {
+        for (var i = 0; i < data[1].length; i++) {
+            //Build the components of each new div
+            var title = $("<h2/>", {
+                text: data[1][i]
+            });
+            var summary = $("<p/>", {
+                text: data[2][i]
+            });
+            var url = data[3][i];
 
-    //Populate with new results
-  $("#search-results").append(
-      $("<div/>", {class: "result animated bounceInLeft"})
-          .append(title, summary)
-        .on("click", clickDivFollowLink(url))
-      );
-  }
+            //Populate with new results
+            $("#search-results").append(
+                $("<div/>", {
+                    class: "result animated bounceInLeft"
+                })
+                .append(title, summary)
+                .on("click", clickDivFollowLink(url))
+            );
+        }
+    }
 }
 
 $(document).ready(
-  $("#search-box").submit(function(event){
-    event.preventDefault();
-    searchWiki();
-  })
+    $("#search-box").submit(function(event) {
+        event.preventDefault();
+        searchWiki();
+    })
 );
